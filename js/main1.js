ab23a7a5803438b54256751e4ff9c01aaa587504
Booldook <booldook@gmail.com>
2018-10-10 14:57:06 +0900

    181010-01

---
+++ js/main1.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDGfj5JyuPXXHoXyX48QJ5I_S0Hccy5LuM",
+	authDomain: "booldook-db1.firebaseapp.com",
+	databaseURL: "https://booldook-db1.firebaseio.com",
+	projectId: "booldook-db1",
+	storageBucket: "booldook-db1.appspot.com",
+	messagingSenderId: "310840275351"
+};
+firebase.initializeApp(config);
